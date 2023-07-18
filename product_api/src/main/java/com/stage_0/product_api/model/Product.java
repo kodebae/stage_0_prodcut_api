35cc5f60ada4ee86372448ed47357bd06401857b
kodebae <heykarmendurbin@gmail.com>
2023-07-17 22:03:05 -0400

    chore: syntax error

--- product_api/src/main/java/com/stage_0/product_api/model/Product.java
+++ product_api/src/main/java/com/stage_0/product_api/model/Product.java
@@ -1,19 +1,16 @@
 package com.stage_0.product_api.model;
-
-public class Product {
 import java.util.UUID;
 
-    public class Product {
-        private UUID id;
-        private String name;
-        private String bigIdea;
-        private String businessCase;
-        private String status;
+public class Product {
+    private UUID id;
+    private String name;
+    private String bigIdea;
+    private String businessCase;
+    private String status;
 
-        // Add other properties
+    // Add other properties
 
-        // Add getters and setters for all properties
-        // Add any additional constructors or methods
+    // Add getters and setters for all properties
+    // Add any additional constructors or methods
 
-    }
 }
